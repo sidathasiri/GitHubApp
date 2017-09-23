91d8f72ac1659e49b87343f95049481a6e907e8b
Sidath <sidath.14@cse.mrt.ac.lk>
2017-09-23 11:10:33 +0530

    added button and input actions

--- Components/Login/login.js
+++ Components/Login/login.js
@@ -1,36 +1,68 @@
 import React, { Component } from 'react';
-import {AppRegistry,StyleSheet,Text,View,Image,TextInput,TouchableHighlight} from 'react-native';
+import {AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    TouchableHighlight,
+    Alert,
+    ActivityIndicator
+} 
+from 'react-native';
 
 export default class Login extends Component {
-  render() {
-    return (
-      <View style = {styles.container}>
-        <Image
-        style={styles.logo}
-        source={require('../../resources/images/githublogo.png')}
-         />
 
-         <Text style={styles.welcomeText}>
-         GitHub Mobile App
-        </Text>
+    constructor(props){
+        super(props);   
+        this.state = {
+            showProgress: false
+        }
+    }
 
-        <TextInput 
-        style={styles.inputBox}
-        placeholder='Username'
-        />
+    render() {
+        return (
+        <View style = {styles.container}>
+            <Image
+            style={styles.logo}
+            source={require('../../resources/images/githublogo.png')}
+            />
 
-        <TextInput 
-        style={styles.inputBox}
-        placeholder='Password'
-        secureTextEntry={true}
-        />
+            <Text style={styles.welcomeText}>
+            GitHub Mobile App
+            </Text>
 
-        <TouchableHighlight style={styles.btn}>
-            <Text style={styles.btnText}>Login</Text>
-        </TouchableHighlight>
-      </View>
-    );
+            <TextInput 
+            style={styles.inputBox}
+            placeholder='Username'
+            onChangeText={(text)=>this.setState({username: text})}
+            />
+
+            <TextInput 
+            style={styles.inputBox}
+            placeholder='Password'
+            secureTextEntry={true}
+            onChangeText={(text)=>this.setState({password: text})}
+            />
+
+            <TouchableHighlight style={styles.btn} onPress={this.loginPressed.bind(this)}>
+                <Text style={styles.btnText}>Login</Text>
+            </TouchableHighlight>
+
+            <ActivityIndicator 
+            style={styles.loader}
+            animating = {this.state.showProgress}
+            size = "large"
+            />
+        </View>
+        );
   }
+
+  loginPressed(){
+      this.setState({showProgress: true});
+      console.log(this.state.showProgress);
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -41,16 +73,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 15
     },
-
     logo: {
         width: 150,
         height: 50,
     },
-
     welcomeText: {
         marginBottom: 15
     },
-
     inputBox: {
         width: 200,
         height: 40,
@@ -59,7 +88,6 @@ const styles = StyleSheet.create({
         marginTop: 13,
         padding: 8,        
     },
-
     btn: {
         backgroundColor: '#268ce0',
         marginTop: 10,
@@ -67,11 +95,12 @@ const styles = StyleSheet.create({
         width: 200,
         justifyContent: 'center'
     },
-
     btnText: {
         color: '#deedf9',
         alignSelf: 'center'
-
+    },
+    loader: {
+        marginTop: 20
     }
 });
 
